4f3e7dbdc187332c84f3cf7409bfce383e4f80cf
Thibaut Selingue <7049020+thislg@users.noreply.github.com>
2025-06-30 09:02:54 +0200

    test(transliterator): add tests

---
+++ Tests/Util/TransliteratorTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace LePhare\Import\Util;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \LePhare\Import\Util\Transliterator
+ *
+ * @internal
+ */
+class TransliteratorTest extends TestCase
+{
+    /**
+     * @dataProvider provideUrlize
+     */
+    public function testUrlize(string $input, string $expected): void
+    {
+        $this->assertEquals($expected, Transliterator::urlize($input));
+    }
+
+    public static function provideUrlize(): iterable
+    {
+        yield ['foo', 'foo'];
+        yield ['FOO', 'foo'];
+        yield ['Hello World', 'hello-world'];
+        yield ['Hello World!', 'hello-world'];
+        yield ['hello world', 'hello-world'];
+        yield ['hello-world', 'hello-world'];
+        yield ['hello_world', 'hello-world'];
+        yield ['hello world!', 'hello-world'];
+        yield ['helloWorld', 'helloworld'];
+        yield ['HelloWorld', 'helloworld'];
+    }
+}
